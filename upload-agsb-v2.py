cbd31ff3ba0b259752496abfd7dfd3dfbd59a436
check rose <128441425+checkxichao@users.noreply.github.com>
2025-06-06 13:10:19 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "xichao"         # 用户名
+UUID = "2e5222bf-004e-4aac-8743-0d6eacb1de46"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "rosecheck.ggff.net"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYzRhYWE3Y2EwOWI4MTQ1NTZhNGQ4MGU4NWVjNjA0MTQiLCJ0IjoiZjQzOTA5MTQtMDU4OS00OWE1LTlmYzQtNzU4NWMyZGE4MTg1IiwicyI6IlpEaGtOR0ZpT0RFdFpXWTFNaTAwTW1NMExXRTNNV1V0TjJVM09XRmlNelZsTmpGbSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
